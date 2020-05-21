a7d08a7855bdac0b8e8e5b17832f1c155fa6991d
Cleberson F R Junior <cleberson.dev@gmail.com>
2020-05-21 20:28:43 -0300

    Refatorado, utilizando async/await

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const axios = require('axios');
 
 
 
-const url = 'https://www.hltv.org/results';
+async function main() {
+  let url = 'https://www.hltv.org/results';
+  const { data } = await axios.get(url);
 
-axios
-  .get(url)
-  .then(scrape)
-  .catch(console.error);
-
-function scrape({ data }) {
   const $ = cheerio.load(data);
   let teams = $('td.team-cell div.team');
+  
   teams = Array.from(teams);
   teams = [...new Set(teams.map(team => team.children[0].data))];
-
   teams.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0));
-  console.log(teams);
 }
+
+main();
