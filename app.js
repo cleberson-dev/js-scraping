73ada8fc672532fcbbdbbc482535c2bc1d09674f
Cleberson F R Junior <cleberson.dev@gmail.com>
2020-05-21 22:34:18 -0300

    Refatorado para melhor legibilidade

--- app.js
+++ app.js
@@ -9,15 +9,14 @@ async function main() {
   // A quantidade de partidas foi obtida manualmente (TODO: Obter automaticamente)
   for (let i = 0; i < 52976; i += 100) {
     // Usando a página de resultados de partidas onde informações do nome do time do site estão disponíveis
-    let url = `https://www.hltv.org/results?offset=${i}`;
+    const url = `https://www.hltv.org/results?offset=${i}`;
     
     console.log(`[${new Date().toUTCString()}] Scraping: ${url}`);
 
     const { data } = await axios.get(url);
-
     const $ = cheerio.load(data);
+
     let newTeams = $('td.team-cell div.team');
-    
     newTeams = Array.from(newTeams);
     newTeams = newTeams.map(team => team.children[0].data);
     newTeams = [... new Set(newTeams)];
@@ -27,6 +26,7 @@ async function main() {
     console.log(`Successfully extracted ${newTeams.length} new teams.`);
   }
 
+  // Os dados persistidos no arquivo não fecham a array com ']'
   fs.appendFile(filename, ']', (err) => {
     if (err) console.error(err);
   });
@@ -37,12 +37,10 @@ async function persist(arr, filename) {
   let stringifiedArray = JSON.stringify(arr).slice(0, -1);
   
   if (!fs.existsSync(filepath)) {
-    const writableFileStream = fs.createWriteStream(filepath);
-    return writableFileStream.write(stringifiedArray);
+    return fs.createWriteStream(filepath).write(stringifiedArray);
   }
 
-  stringifiedArray = stringifiedArray.replace('[', ',');
-  fs.appendFile(filepath, stringifiedArray, (err) => {
+  fs.appendFile(filepath, stringifiedArray.replace('[', ','), (err) => {
     if (err) console.error(err);
   });
 }
